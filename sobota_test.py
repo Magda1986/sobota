8f44bea623aa65364fbbe5d7e112e25cb6b168ca
Magda Ratajczak <magda.ratajczak1986@gmail.com>
2022-05-28 10:30:13 +0200

    Add Kolejny warunek

--- sobota_test.py
+++ sobota_test.py
@@ -7,7 +7,10 @@ print(lista)
 lista.append("sok pomarańczowy")
 print(lista)
 
-x = "Pada deszcz"
+#x = "Pada deszcz"
+x = "Świeci słońce"
 
 if x == "Pada deszcz":
     print("Koniecznie zabierz parasol idąć do sklepu!")
+else:
+    print("Pogoda za oknem ok, moesz iść bez parasola :)")
