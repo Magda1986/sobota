096809949ca046372c5f6400f7708a21119b161d
Magda1986 <104586924+Magda1986@users.noreply.github.com>
2022-05-28 10:33:33 +0200

    Update sobota_test.py
    Uzupełniona lista zakupów

--- sobota_test.py
+++ sobota_test.py
@@ -6,6 +6,7 @@ lista.append("masło")
 print(lista)
 lista.append("sok pomarańczowy")
 print(lista)
+lista. append("pepsi")
 
 #x = "Pada deszcz"
 x = "Świeci słońce"
@@ -13,4 +14,4 @@ x = "Świeci słońce"
 if x == "Pada deszcz":
     print("Koniecznie zabierz parasol idąć do sklepu!")
 else:
-    print("Pogoda za oknem ok, moesz iść bez parasola :)")
+    print("Pogoda za oknem ok, możesz iść bez parasola :)")
